88c4f15dc5a1b945da684492589bc0781dfac06d
xboard <flavio.arruda@gmail.com>
2020-12-08 11:15:22 -0300

    Task 2 solved

--- 2020/Day_8/handheld_halting.py
+++ 2020/Day_8/handheld_halting.py
@@ -9,7 +9,7 @@ import argparse
 import gzip
 from os.path import dirname, realpath
 from io import StringIO
-from typing import IO, Iterator
+from typing import IO, Iterator, Sequence, Tuple
 from pathlib import Path
 from enum import Enum
 from dataclasses import dataclass
@@ -33,7 +33,7 @@ class Operation(Enum):
         raise NameError(f"{oper} is not a valid {cls}")
 
 
-@dataclass(frozen=True)
+@dataclass
 class Instruction:
     """Program instruction representation."""
 
@@ -48,19 +48,29 @@ def read_instructions(input_io: IO) -> Iterator[Instruction]:
         yield Instruction(Operation.from_string(line[:3]), int(line[3:]))
 
 
-def task1(input_io: IO) -> int:
+def run_until_loop_or_end(program: Sequence[Instruction]) -> Tuple[int, bool]:
     """
-    Solve task 1.
+    Interpret program until the end or finds a loop.
 
     Parameters
     ----------
-    input_io: IO
-        stream to all .
+    program: Sequence[Instruction]
+        sequence of program instructions.
 
     Return
     ------
-    int
-        .
+    Tuple[int, bool]
+        int: last value of acc.
+        bool: True if found loop.
+              False if program was interpreted until the end.
+
+    Raises
+    ------
+    Exception
+        if no loop in found in given program.
+        This is not suposed to happen with given AoC input.
+    NotImplementedError
+        if program has instruction not implemented.
 
     """
     # accumulator value
@@ -70,10 +80,8 @@ def task1(input_io: IO) -> int:
     # record of previous instruction pointer values to find repeatition.
     ip_seen = set()
     looping = False
-    # program.
-    program = tuple(read_instructions(input_io))
 
-    while not looping:
+    while not looping and inst_ptr < len(program):
         if inst_ptr in ip_seen:
             looping = True
             continue
@@ -91,10 +99,36 @@ def task1(input_io: IO) -> int:
                 f"Task 1 doesn't know how to handle op={instruction.oper}"
             )
 
-    if not looping:
-        raise Exception(f"Invalid state: program finished without looping. acc = {acc}")
+    return acc, looping
 
-    return acc
+
+def task1(input_io: IO) -> int:
+    """
+    Solve task 1.
+
+    Parameters
+    ----------
+    input_io: IO
+        stream to program.
+
+    Return
+    ------
+    int
+        value of acc before looping.
+
+    Raises
+    ------
+    Exception
+        if no loop in found in given program.
+        This is not suposed to happen with given AoC input.
+    NotImplementedError
+        if program has instruction not implemented.
+    """
+    program = tuple(read_instructions(input_io))
+    result, looping = run_until_loop_or_end(program)
+    if not looping:
+        raise Exception("Invalid state: program has no loop")
+    return result
 
 
 def task2(input_io: IO) -> int:
@@ -104,15 +138,32 @@ def task2(input_io: IO) -> int:
     Parameters
     ----------
     input_io: IO
-        stream to all .
+        stream to program.
 
     Return
     ------
     int
-        .
+        value of acc after fix and program finishes.
 
     """
-    pass
+    program = tuple(read_instructions(input_io))
+    for idx, instr in enumerate(program):
+
+        modified_program = None
+
+        if instr.oper in (Operation.NOP, Operation.JMP):
+            modified_program = list(program)
+            if instr.oper == Operation.NOP:
+                modified_program[idx] = Instruction(Operation.JMP, instr.value)
+            else:
+                modified_program[idx] = Instruction(Operation.NOP, instr.value)
+
+        if modified_program:
+            result, looping = run_until_loop_or_end(modified_program)
+            if not looping:
+                return result
+
+    raise Exception("Invalid state: program still loops.")
 
 
 def get_input_file() -> Path:
@@ -137,8 +188,8 @@ def main() -> None:
         print(f"Task 1: acc = {acc} before looping.")
 
     with gzip.open(input_file, "rt", encoding="ascii") as file:
-        questions_solved = task2(file)
-        print(f"Task 2: answer is {questions_solved}.")
+        acc = task2(file)
+        print(f"Task 2: acc = {acc} after fix.")
 
 
 if __name__ == "__main__":
@@ -165,6 +216,7 @@ acc +6"""
 
 
 def test_read_instructions():
+    """Test function read_instructions with problem example."""
     instructions = read_instructions(input_stream())
     assert next(instructions) == Instruction(Operation.NOP, 0)
     assert next(instructions) == Instruction(Operation.ACC, 1)
@@ -175,17 +227,17 @@ def test_read_instructions():
     assert next(instructions) == Instruction(Operation.ACC, 1)
     assert next(instructions) == Instruction(Operation.JMP, -4)
     assert next(instructions) == Instruction(Operation.ACC, 6)
-    assert next(instructions, None) == None
+    assert next(instructions, None) is None
 
 
 def test_task1_with_example_input():
-    """Test task1."""
+    """Test task1 with input from problem example."""
     acc = task1(input_stream())
     assert acc == 5
 
 
 def test_task2_with_example_input():
-    """Test task2."""
+    """Test task2 with example problem example."""
     acc = task2(input_stream())
     assert acc == 8
 
@@ -194,11 +246,11 @@ def test_task1_with_input_file():
     """Test task1 with given input file (gziped)."""
     with gzip.open(INPUT_FILE_PATH, "rt", encoding="ascii") as file:
         acc = task1(file)
-        # assert val == 777
+        assert acc == 1217
 
 
 def test_task2_with_input_file():
     """Test task2 with given input file (gziped)."""
     with gzip.open(INPUT_FILE_PATH, "rt", encoding="ascii") as file:
-        val = task2(file)
-        # assert val == 777
+        acc = task2(file)
+        assert acc == 501
