9b64007f08137ad496630d36cc3a385ef3dffaa5
xboard <flavio.arruda@gmail.com>
2020-12-18 09:51:25 -0300

    Task 2 solved

--- 2020/Day_17/conway_cubes.py
+++ 2020/Day_17/conway_cubes.py
@@ -9,85 +9,105 @@ import argparse
 import gzip
 from os.path import dirname, realpath
 from io import StringIO
-from typing import Tuple, List, Set, IO, Iterator, NewType, cast
+from typing import Tuple, IO, Set
 from pathlib import Path
-from dataclasses import dataclass
+from enum import Enum, auto
 
 INPUT_FILE_PATH = Path(dirname(realpath(__file__))) / "input.txt.gz"
 
-State = NewType("State", Set[Tuple[int, int, int]])
+Dimension = Tuple[int, int, int, int]
+State = Set[Dimension]
+
+
+class Task(Enum):
+    """Enumerate tasks."""
+
+    Task1 = auto()
+    Task2 = auto()
 
 
 def read_initial_state(input_io: IO) -> State:
+    """
+    Read initial state
+
+    Parameters
+    ----------
+    input_io: IO
+        stream to initial state.
+
+    Return
+    ------
+    State:
+        initial State.
+    """
     row = 0
     actives = set()
     while line := input_io.readline():
         for col, char in enumerate(line.strip()):
             if char == "#":
-                actives.add((row, col, 0))
+                actives.add((row, col, 0, 0))
         row += 1
-    return State(actives)
+    return actives
 
 
-def task1(input_io: IO) -> int:
+def dim_iterator(task: Task) -> Dimension:
+    """Iterates of variable dimensions"""
+    for dx in [-1, 0, 1]:
+        for dy in [-1, 0, 1]:
+            for dz in [-1, 0, 1]:
+                if task == Task.Task2:
+                    for dw in [-1, 0, 1]:
+                        yield (dx, dy, dz, dw)
+                else:
+                        yield (dx, dy, dz, 0)
+
+
+def solve(input_io: IO, task: Task) -> int:
     """
     Solve task 1.
 
     Parameters
     ----------
     input_io: IO
-        stream to all .
+        stream to initial state.
 
     Return
     ------
     int
-        .
+        number of active cubes.
 
     """
     state = read_initial_state(input_io)
     for _ in range(6):
-        candidates = State(set())
-        for x, y, z in state:
+        candidates: State = set()
+        for x, y, z, w in state:
             for dx in [-1, 0, 1]:
                 for dy in [-1, 0, 1]:
                     for dz in [-1, 0, 1]:
-                        candidates.add((x + dx, y + dy, z + dz))
+                        if task == Task.Task2:
+                            for dw in [-1, 0, 1]:
+                                candidates.add((x + dx, y + dy, z + dz, w + dw))
+                        else:
+                            candidates.add((x + dx, y + dy, z + dz, 0))
 
-        actives = State(set())
-        for x, y, z in candidates:
+        actives: State = set()
+        for x, y, z, w in candidates:
             num_actives = 0
             for dx in [-1, 0, 1]:
                 for dy in [-1, 0, 1]:
                     for dz in [-1, 0, 1]:
-                        if dx == dy == dz == 0:
-                            continue
-                        if (x + dx, y + dy, z + dz) in state:
-                            num_actives += 1
+                        for dw in [-1, 0, 1]:
+                            if dx == dy == dz == dw == 0:
+                                continue
+                            if (x + dx, y + dy, z + dz, w + dw) in state:
+                                num_actives += 1
 
-            if (x, y, z) in state and num_actives in [2, 3]:
-                actives.add((x, y, z))
-            if (x, y, z) not in state and num_actives == 3:
-                actives.add((x, y, z))
-        state = State(actives)
+            if (x, y, z, w) in state and num_actives in [2, 3]:
+                actives.add((x, y, z, w))
+            if (x, y, z, w) not in state and num_actives == 3:
+                actives.add((x, y, z, w))
+        state = actives
     return len(state)
-
-
-def task2(input_io: IO) -> int:
-    """
-    Solve task 2.
-
-    Parameters
-    ----------
-    input_io: IO
-        stream to all .
-
-    Return
-    ------
-    int
-        .
-
-    """
-    pass
 
 
 def get_input_file() -> Path:
@@ -108,11 +128,11 @@ def main() -> None:
     """Run script."""
     input_file = get_input_file()
     with gzip.open(input_file, "rt", encoding="ascii") as file:
-        answer = task1(file)
+        answer = solve(file, Task.Task1)
         print(f"Task 1: answer is {answer}.")
 
     with gzip.open(input_file, "rt", encoding="ascii") as file:
-        answer = task2(file)
+        answer = solve(file, Task.Task2)
         print(f"Task 2: answer is {answer}.")
 
 
@@ -134,36 +154,46 @@ def input_stream() -> IO:
 
 
 def test_read_cubes():
+    """Test read_cubes function."""
     state = read_initial_state(input_stream())
     assert len(state) == 5
-    assert (1, 2, 0) in state
-    assert (0, 0, 0) not in state
-    assert (0, 1, 0) in state
-    assert (1, 1, 0) not in state
-    assert (2, 1, 0) in state
+    assert (1, 2, 0, 0) in state
+    assert (0, 0, 0, 0) not in state
+    assert (0, 1, 0, 0) in state
+    assert (1, 1, 0, 0) not in state
+    assert (2, 1, 0, 0) in state
+
+def test_dim_iterator():
+    """ Test dim_iterator function """
+    iter = tuple(dim_iterator(Task.Task1))
+    assert len(iter) == 27
+    assert iter[0] == (-1, -1, -1, 0)
+    iter = tuple(dim_iterator(Task.Task2))
+    assert len(iter) == 81
+    assert iter[0] == (-1, -1, -1, -1)
 
 
 def test_task1_with_example_input():
     """Test task1 with problem statement example."""
-    answer = task1(input_stream())
+    answer = solve(input_stream(), Task.Task1)
     assert answer == 112
 
 
-# def test_task2_with_example_input():
-#     """Test task2 with problem statement example."""
-#     questions_solved = task2(input_stream())
-#     assert questions_solved == 6
+def test_task2_with_example_input():
+    """Test task2 with problem statement example."""
+    answer = solve(input_stream(), Task.Task2)
+    assert answer == 848
 
 
 def test_task1_with_input_file():
     """Test task1 with given input file (gziped)."""
     with gzip.open(INPUT_FILE_PATH, "rt", encoding="ascii") as file:
-        answer = task1(file)
+        answer = solve(file, Task.Task1)
         assert answer == 426
 
 
-# def test_task2_with_input_file():
-#     """Test task2 with given input file (gziped)."""
-#     with gzip.open(INPUT_FILE_PATH, "rt", encoding="ascii") as file:
-#         val = task2(file)
-#         assert val == 777
+def test_task2_with_input_file():
+    """Test task2 with given input file (gziped)."""
+    with gzip.open(INPUT_FILE_PATH, "rt", encoding="ascii") as file:
+        answer = solve(file, Task.Task2)
+        assert answer == 1892
